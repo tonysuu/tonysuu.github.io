ab86a91990678ca9b705bc851b09c70aa9a96e07
tonysuu <tonysu1122@hotmail.com>
2015-06-05 16:43:37 -0400

    updated website layout

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // JavaScript source code
+$(document).ready(function () {
+    $('.icon').mouseenter(function () {
+        $(this).fadeTo("fast", 1);
+    });
+    $('.icon').mouseleave(function () {
+        $(this).fadeTo("fast",0.7);
+    });
+});
