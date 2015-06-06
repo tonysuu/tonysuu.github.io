b74384129d10e8fa9fc5c7879da0132fa927835b
tonysuu <tonysu1122@hotmail.com>
2015-06-05 22:20:37 -0400

    working menu toggle

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 // JavaScript source code
 $(document).ready(function () {
+    //highlight icon
     $('.icon').mouseenter(function () {
         $(this).fadeTo("fast", 1);
     });
     $('.icon').mouseleave(function () {
         $(this).fadeTo("fast",0.7);
+    });
+    //display menu
+    $('#menu-icon').click(function () {
+        $('body').animate({
+            left: "300px"
+        }, 200);
+        $('.menu').animate({
+            left: "0px"
+        }, 200);
+    });
+    //close menu
+    $("#exit").click(function () {
+        $('body').animate({
+            left: "0px"
+        },200);
+        $('.menu').animate({
+            left: "-300px"
+        },200);
     });
 });
