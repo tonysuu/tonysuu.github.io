4083af01a901c154b049b07eea52e991c4f4c7d4
tonysuu <tong.su@TOR-TSU-LW7.na.audatex.com>
2015-07-10 14:08:30 -0400

    scrollbar not working exiting applet

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 // JavaScript source code
+var appletId;
+
 if (navigator.userAgent.indexOf("Chrome") < 0 && navigator.userAgent.indexOf("Firefox") < 0) {
     alert("This website is optimized for Chrome and Firefox, some functionalities may not work in IE");
 }
-
+var hide = function () {
+    document.getElementById('applet_container').removeChild(document.getElementById(appletId));
+    document.getElementById('overlay').style.zIndex = -1;
+    document.getElementById('overlay').style.visibility = "hidden";
+}
+var show = function (id,arcName,clsName,height,width) {
+    document.getElementById('overlay').style.zIndex = 1;
+    document.getElementById('overlay').style.visibility = "visible";
+    var top = window.innerHeight / 2 - height / 2;
+    var left = window.innerWidth / 2 - width / 2;
+    document.getElementById('overlay_content').style.top = top + "px";
+    document.getElementById('overlay_content').style.left = left + "px";
+    var app = document.createElement('applet');
+    app.archive = arcName;
+    app.id = id;
+    appletId = id;
+    app.code = clsName;
+    app.height = height;
+    app.width = width;
+    document.getElementById('applet_container').appendChild(app);
+}
 $(document).ready(function () {
     //highlight icon
+    var dHeight = $(document).height();
+    $('#overlay').css("height", dHeight + "px");
     $('.icon').mouseenter(function () {
         /*$(this).fadeTo("fast", 1);*/
         $(this).css("background-color", "white");
@@ -56,6 +80,5 @@ $(document).ready(function () {
         }, 0, moveToLeft($('body').width()));
     }
     moveToLeft($('body').width());
-    
 });
 
