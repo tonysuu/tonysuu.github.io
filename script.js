d1f39447cf9f56b15374263f55bbf4e8c9fea73f
tonysuu <tong.su@TOR-TSU-LW7.na.audatex.com>
2015-07-27 16:55:11 -0400

    fixed menu positioning

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
         //location.assign("index.html");
     });
     //close menu
-    $('#close-image').click(function () {
+    $('img','#close-button').click(function () {
         $('body').animate({
             left: "0px"
         }, 200);
@@ -76,10 +76,10 @@ $(document).ready(function () {
     });
     $('#accordion').accordion({ collapsible: true, active: false });
     //first item
-    $('#ui-id-1').unbind("click");
+    /*$('#ui-id-1').unbind("click");
     $('#ui-id-1').click(function () {
         location.assign("index.html");
-    });
+    });*/
     //resume tab
     $('#resume').unbind("click");
     //contacts tab
