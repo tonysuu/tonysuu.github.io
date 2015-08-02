f0327a1370b765cc194d34b8eb55ea6c52fe9fef
tonysuu <tonysu1122@hotmail.com>
2015-08-01 21:29:58 -0400

    workign email

--- mailTo.php
+++ mailTo.php
@@ -4,10 +4,10 @@
     $subject="Sent From tonysu.me";
     $sender=$_POST["sender"];
     $senderEmail=$_POST["sender"];
-    $message=$_POST["message"]
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-    mail($recipient,$subject,$mailBody,"From: $sender <$senderEmail>");
+    $message=$_POST["message"];
+    $mailBody="Name";
+    mail($recipient,$subject,$mailBody);
     sleep(1);
-	header("Location:http://tonysu.me/contacts.html"); // Set here redirect page or destination page
+	header('Location: contacts.html'); // Set here redirect page or destination page
   }
 ?>
